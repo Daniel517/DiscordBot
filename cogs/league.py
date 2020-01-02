434aaa19c9c332491406fb27a60674dde5cf99bd
Daniel517 <ramirezdaniel51@yahoo.com>
2020-01-02 08:40:22 -0800

    Changed place of try except in live command. Try is used when creating image.

--- cogs/league.py
+++ cogs/league.py
@@ -29,31 +29,31 @@ class League(commands.Cog):
 		# Sends a loading gif for users to know command is being processed
 		await ctx.message.channel.send(file = discord.File('./staticdata/load01.gif', 'load.gif'))
 		summoner_account_data = LeagueAPI.get_summoner_account_info(summoner_name_str)
-		try:
-			#Checks if there was an error getting summoner account data
-			if 'error' in summoner_account_data:
+		#Checks if there was an error getting summoner account data
+		if 'error' in summoner_account_data:
+			#Removes loading gif and then returns an error message
+			await ctx.channel.purge(limit=1)
+			await ctx.send(summoner_account_data['error'])
+			return
+		elif summoner_account_data:
+			summoner_id = summoner_account_data['id']
+			live_match_data = LeagueAPI.get_live_match_data(summoner_id)
+			#Checks if there was an error getting live match data
+			if 'error' in live_match_data:
 				#Removes loading gif and then returns an error message
 				await ctx.channel.purge(limit=1)
-				await ctx.send(summoner_account_data['error'])
+				await ctx.send(live_match_data['error'])
 				return
-			elif summoner_account_data:
-				summoner_id = summoner_account_data['id']
-				live_match_data = LeagueAPI.get_live_match_data(summoner_id)
-				#Checks if there was an error getting live match data
-				if 'error' in live_match_data:
-					#Removes loading gif and then returns an error message
-					await ctx.channel.purge(limit=1)
-					await ctx.send(live_match_data['error'])
-					return
-				#If no error, checks if image already stored in imgs folder(used to remove unnecessary reprocessing of images for same game)
-				elif os.path.exists(f'./imgs/{live_match_data["game_id"]}.png'):
-					#Removes loading gif and then returns the already stored imge
-					await ctx.channel.purge(limit=1)
-					img_path = f'./imgs/{live_match_data["game_id"]}.png'
-					await ctx.message.channel.send(file = discord.File(img_path, 'match.png'))
-					return
-				#If no error and image not already in imgs folder
-				elif live_match_data:
+			#If no error, checks if image already stored in imgs folder(used to remove unnecessary reprocessing of images for same game)
+			elif os.path.exists(f'./imgs/{live_match_data["game_id"]}.png'):
+				#Removes loading gif and then returns the already stored imge
+				await ctx.channel.purge(limit=1)
+				img_path = f'./imgs/{live_match_data["game_id"]}.png'
+				await ctx.message.channel.send(file = discord.File(img_path, 'match.png'))
+				return
+			#If no error and image not already in imgs folder
+			elif live_match_data:
+				try:
 					game_id = live_match_data['game_id']
 					summoners = live_match_data['summoners']
 					banned_champs = live_match_data['banned_champs']
@@ -65,9 +65,9 @@ class League(commands.Cog):
 					#CHANGE TO WHILE LOOP AND CHECK EVERY 3 MIN IF GAME STILL GOING ON, IF NOT DELETE IMG
 					await asyncio.sleep(60)
 					os.remove(image_path)
-		except:
-			await ctx.channel.purge(limit=1)
-			await ctx.send('An error occured getting live match.')
+				except:
+					await ctx.channel.purge(limit=1)
+					await ctx.send('An error occured getting image for live match.')
 
 
 def setup(client):
